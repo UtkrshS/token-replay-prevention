59b168bf64a958821be069dbf4bca3ae28e4a54b
Utkarsh Sharma <sharmautkarsh96@gmail.com>
2024-08-01 21:43:21 +0530

    Create TokenReplayPreventionApplication.java

---
+++ src/main/java/com/ping/identity/token/TokenReplayPreventionApplication.java
@@ -0,0 +1,11 @@
+package com.pingidentity.token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenReplayPreventionApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TokenReplayPreventionApplication.class, args);
+    }
+}
